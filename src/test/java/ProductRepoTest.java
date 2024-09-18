2b044e5bb2de40686f38cefb81bb05f94e52f005
ursulamontag <montagu666@gmail.com>
2024-09-18 11:10:45 +0200

    Make getOrderById return Optional and adjust implementations

--- src/test/java/ProductRepoTest.java
+++ src/test/java/ProductRepoTest.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ProductRepoTest {
 
